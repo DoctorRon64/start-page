2d2050aa97ebe409f31d13371125477c87e0eb7d
DoctorRon64 <DoctorRon64@users.noreply.github.com>
2024-07-01 20:40:08 +0200

    style and links update

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,14 +1,32 @@
 ﻿document.addEventListener('DOMContentLoaded', () => {
+    function updateClock() {
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        const seconds = now.getSeconds().toString().padStart(2, '0');
+        const timeString = `${hours}:${minutes}:${seconds}`;
+
+        const clockElement = document.getElementById('clock');
+        if (clockElement) {
+            clockElement.textContent = timeString;
+        }
+    }
+    updateClock();
+    setInterval(updateClock, 1000);
+    
+    
     fetch('/json/links.json')
         .then(response => response.json())
         .then(data => {
-            let htmlContent = '';
+            let htmlContent = '<div class="sections-container">';
             data.sections.forEach(section => {
-                htmlContent += `<h2>${section.name}</h2>`;
+                htmlContent += `<div class="section"><h2>${section.name}</h2>`;
                 section.links.forEach(link => {
                     htmlContent += returnHTML(link);
                 });
+                htmlContent += '</div>';
             });
+            htmlContent += '</div>';
 
             document.getElementById('links').innerHTML = htmlContent;
         })
@@ -33,7 +51,7 @@
 function returnHTML(link) {
     return `
         <a href="${link.url}" title="${link.hovertext}" class="link">
-            <iconify-icon icon="${link.icon}" width="24" height="24" style="transform: translateY(0px);"></iconify-icon>
+            <iconify-icon icon="${link.icon}" width="24" height="24" style="transform: translateY(0px); margin: 3px;"></iconify-icon>    
             <span>${link.name}</span>
         </a>
     `;
